7e593705178bee8587f70bf7dbc6ffd30f948dd7
thifelipesilva <thiagophsilva@hotmail.com>
2023-02-01 01:12:48 -0300

    Iniciando aplicacao mvc com php

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SK8-Play</title>
+</head>
+
+<body>
+
+    <header>
+
+        <nav>
+            <h1>Sk8-Play</h1>
+
+            <div>
+                <a href="./pages/enviar-video.html" >Adicionar Videos</a>
+                <a href="./pages/login.html">Sair</a>
+            </div>
+        </nav>
+
+    </header>
+
+    <ul alt="videos">
+        <li>
+            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/Hap7L8YkMDM"
+                title="Video de skate" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div>
+                
+                <h3>Melhor video de skate</h3>
+                <div>
+                    <a href="./pages/enviar-video.html">Editar</a>
+                    <a href="./pages/enviar-video.html">Excluir</a>
+                </div>
+            </div>
+        </li>
+        
+        <li>
+            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/Hap7L8YkMDM"
+                title="Video de skate" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div>
+                
+                <h3>Melhor video de skate</h3>
+                <div>
+                    <a href="./pages/enviar-video.html">Editar</a>
+                    <a href="./pages/enviar-video.html">Excluir</a>
+                </div>
+            </div>
+        </li>
+
+        <li>
+            <iframe width="100%" height="72%" src="https://www.youtube.com/embed/Hap7L8YkMDM"
+                title="Video de skate" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>
+            <div>
+                
+                <h3>Melhor video de skate</h3>
+                <div>
+                    <a href="./pages/enviar-video.html">Editar</a>
+                    <a href="./pages/enviar-video.html">Excluir</a>
+                </div>
+            </div>
+        </li>
+
+    </ul>
+</body>
+
+</html>
